9531dcbbd21d793825f18ad24fcaec8589e1984a
Marcos Borges <marcosvgb13.mb@gmail.com>
2022-07-27 22:27:40 -0300

    initial commit

---
+++ src/main/java/com/example/MicroTopicos/MicroTopicosApplication.java
@@ -0,0 +1,13 @@
+package com.example.MicroTopicos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroTopicosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroTopicosApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/example/model/Produtos.java
@@ -0,0 +1,106 @@
+package com.example.model;
+
+import java.time.LocalDate;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+
+@Entity
+@Table(name="produtos")
+public class Produtos {
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	private long id;
+	
+	@NotBlank
+	private String nome;
+
+	@NotBlank
+	@Lob
+	private String descricao;
+	
+	@NotBlank
+	private int quantidade;
+	
+	@NotBlank
+	private double preco;
+	
+	@NotBlank
+	private boolean ativo;
+	
+	@NotBlank
+	@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd")
+	private LocalDate data_criacao;
+	
+	@NotBlank
+	@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd")
+	private LocalDate data_atualizacao;
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+	public int getQuantidade() {
+		return quantidade;
+	}
+
+	public void setQuantidade(int quantidade) {
+		this.quantidade = quantidade;
+	}
+
+	public double getPreco() {
+		return preco;
+	}
+
+	public void setPreco(double preco) {
+		this.preco = preco;
+	}
+
+	public boolean isAtivo() {
+		return ativo;
+	}
+
+	public void setAtivo(boolean ativo) {
+		this.ativo = ativo;
+	}
+
+	public LocalDate getData_criacao() {
+		return data_criacao;
+	}
+
+	public void setData_criacao(LocalDate data_criacao) {
+		this.data_criacao = data_criacao;
+	}
+
+	public LocalDate getData_atualizacao() {
+		return data_atualizacao;
+	}
+
+	public void setData_atualizacao(LocalDate data_atualizacao) {
+		this.data_atualizacao = data_atualizacao;
+	}
+	
+}
